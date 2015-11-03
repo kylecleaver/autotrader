efccba77a2584be978598278a36c57ecdedbc9dc
unknown <kcleaver@lat-kcleaver1.cb.careerbuilder.com>
2015-11-02 20:52:30 -0500

    Create Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102233535) do
+ActiveRecord::Schema.define(version: 20151103003406) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "make_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "makes", force: true do |t|
     t.string   "name"
